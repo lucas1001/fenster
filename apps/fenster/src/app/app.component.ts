9352398315a1d28b40afc80ed4a125d3563495c1
lucas1001 <cabaj85@gmail.com>
2021-08-23 21:46:13 +0200

    test local api connection

--- apps/fenster/src/app/app.component.ts
+++ apps/fenster/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'fenster-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fenster';
+  value = '';
+
+  constructor(
+    private http: HttpClient
+  ){
+
+  }
+
+  ngOnInit(): void{
+    this.getData().subscribe();
+    }
+
+  getData(){
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return  this.http.get<any>('http://localhost:3333/api')
+    .pipe(
+      map((res) => {
+        console.log(res)
+        this.value = res.message;
+      }),
+     
+    );
+  }
 }
