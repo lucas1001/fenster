852321587a81d0238c9963d3c226bf15168fa6c5
lucas1001 <cabaj85@gmail.com>
2021-08-23 19:37:01 +0200

    initial commit

---
+++ apps/fenster/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fenster-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'fenster';
+}
